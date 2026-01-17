63f811ca27cf2555393773c9a6a06d31f155bafc
ekin04 <mirko.chiaverini@gmail.com>
2026-01-17 16:02:37 +0100

    Typo error

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,18 @@ import { z, defineCollection } from 'astro:content';
 
 const blogPostCollection = defineCollection({
     type: 'content',
-    schema:() =>
-    z.object({
-        title: z.string(),
-        excerpt: z.string(),
-        categoria: z.array(z.string()),
-        date: z.coerce.date(),
-        thumbnail: z.string(),
-        galleria: z.array(z.object({
-            src: z.string(),
-            alt: z.string(),
-        }))
-    }),
+    schema: () =>
+        z.object({
+            title: z.string(),
+            excerpt: z.string(),
+            categoria: z.array(z.string()),
+            date: z.coerce.date(),
+            thumbnail: z.string(),
+            galleria: z.array(z.object({
+                src: z.string(),
+                alt: z.string(),
+            }))
+        }),
 })
 
 const categoryCollection = defineCollection({

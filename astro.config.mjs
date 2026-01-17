925ee3f4bf390c747ad35a216e7d2d46260dd19a
ekin04 <mirko.chiaverini@gmail.com>
2026-01-17 15:42:51 +0100

    Test keystatic cloud2

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,20 +9,23 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
-import node from "@astrojs/node";
-
-let adapter = vercel({ isr: true });
-if (process.argv[3] === "--node" || process.argv[4] === "--node") {
-  adapter = node({ mode: "standalone" });
-}
 
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), icon(), sitemap()],
+  output: "static",
+  integrations: [
+    react(),
+    markdoc(),
+    keystatic(),
+    icon(),
+    sitemap()
+  ],
   site: siteConfig.site.baseUrl,
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter,
+  adapter: vercel({
+    isr: true
+  }),
   redirects: {
     "/digital": "/keystatic",
   },
